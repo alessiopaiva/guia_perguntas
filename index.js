b43b499aecc58ebc5fa8a3a7bfd61bd752403421
Alessio <alessio.bertolini@gmail.com>
2023-11-26 09:19:23 -0300

    bootstrap

--- index.js
+++ index.js
@@ -3,25 +3,14 @@ const app = express()
 
 //express usar EJS como view engine
 app.set('view engine','ejs')
+app.use(express.static('public'))
 
 app.get('/:nome/:lang', (req, res) => {
-    var nome = req.params.nome
-    var lang = req.params.lang
-    var exibirMsg = false
+    res.render('index')
+})
 
-    var produtos = [
-        {nome: 'Biscoito', preco: 3.14},
-        {nome: 'Maça', preco: 5}
-    ]
-
-    res.render('index',{
-        nome: nome,
-        lang: lang,
-        empresa: 'Guia do programador',
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    })
+app.get('/perguntar',(req,res)=>{
+    res.render('perguntar')
 })
 
 app.listen(8080,()=>{console.log('app rodando')})
